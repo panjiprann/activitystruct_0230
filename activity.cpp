32090c78a1bcc6295a394177fd6b79c4fd4a7a7b
panjiprann <panjipranantya20@gmail.com>
2024-04-02 15:33:58 +0700

    output

--- activity.cpp
+++ activity.cpp
@@ -20,4 +20,9 @@ struct mahasiswa
  cin  >> mhs.alamat;
  cout << "umur mahasiswa :";
  cin  >> mhs.umur;
+ cout << endl;
+ cout << "\n nim : "<< mhs.nim;
+ cout << "\n nama : "<< mhs.nama;
+ cout << "\n alamat : "<< mhs.alamat;
+ cout << "\n umur : "<< mhs.umur;
  }
