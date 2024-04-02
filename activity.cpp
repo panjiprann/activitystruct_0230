1207947a228e681401adf5e94d51ccb51af0becc
panjiprann <panjipranantya20@gmail.com>
2024-04-02 16:10:44 +0700

    memasukkan output ke function

--- activity.cpp
+++ activity.cpp
@@ -19,25 +19,24 @@ int main(){
     Mahasiswa mhs[3];
     for (int i = 0; i < 3; i++)
     {
-    
+      mahasiswa mhs;
+    cout << "nomor mahasiswa :";
+    getline(cin,mhs.nim);
+    cout << "nama mahasiswa :";
+    getline(cin,mhs.nama);
+    cout << "alamat mahasiswa :";
+    cout << "\t nama desa = ";
+    cin >> mhs.alamat.desa;
+    cout << "\t nama kota = ";
+    cin >> mhs.alamat.kota;
+    cout << "umur mahasiswa :";
+    cin  >> mhs.umur;
+    cout << endl;
     }
 }
 
  int main() 
  {
- mahasiswa mhs;
- cout << "nomor mahasiswa :";
- getline(cin,mhs.nim);
- cout << "nama mahasiswa :";
- getline(cin,mhs.nama);
- cout << "alamat mahasiswa :";
- cout << "\t nama desa = ";
- cin >> mhs.alamat.desa;
- cout << "\t nama kota = ";
- cin >> mhs.alamat.kota;
- cout << "umur mahasiswa :";
- cin  >> mhs.umur;
- cout << endl;
  cout << "\n nim : "<< mhs.nim;
  cout << "\n nama : "<< mhs.nama;
  cout << "\n alamat : "<< mhs.alamat.desa;
